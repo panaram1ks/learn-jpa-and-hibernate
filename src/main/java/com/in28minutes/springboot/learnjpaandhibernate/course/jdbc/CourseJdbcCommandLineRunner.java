d887fb8beb74d9a2a9fda0a7f031be47b73f6ca3
evgen <parom.evgen@mail.ru>
2023-03-25 18:01:00 +0300

    use springJdbcTemplate

---
+++ src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -0,0 +1,18 @@
+package com.in28minutes.springboot.learnjpaandhibernate.course.jdbc;
+
+import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CourseJdbcCommandLineRunner implements CommandLineRunner {
+
+    @Autowired
+    private CourseJdbcRepository courseJdbcRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        courseJdbcRepository.insert(new Course(1l, "Learn AWS 1", "in28minutes"));
+    }
+}
---
+++ src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
@@ -0,0 +1,25 @@
+package com.in28minutes.springboot.learnjpaandhibernate.course.jdbc;
+
+import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CourseJdbcRepository {
+
+    @Autowired
+    private JdbcTemplate springJdbcTemplate;
+
+    private static String INSERT_QUERY =
+            """
+                insert into course(id, name, author)
+                values(?, ?, ?);          
+            """;
+
+    public void insert(Course course){
+        springJdbcTemplate.update(INSERT_QUERY, course.getId(), course.getName(), course.getAuthor());
+
+    }
+
+}
